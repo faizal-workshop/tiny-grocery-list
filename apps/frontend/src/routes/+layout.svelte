bd0a0e63e165f62399b42875ee1b8ef3581f1ca8
Faizal Chan <moefc32@gmail.com>
2025-02-15 16:08:43 +0700

    add mobile phone mockup

---
+++ apps/frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import "../app.css";
+</script>
+
+<svelte:head>
+  <title>
+    {import.meta.env.VITE_APP_NAME}
+  </title>
+</svelte:head>
+
+<slot />
--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Phone from "$lib/component/Phone.svelte";
+</script>
+
+<main class="flex flex-1 flex-col justify-center items-center p-3">
+  <Phone size={20}>
+    <h1 class="text-3xl">Hello world!</h1>
+  </Phone>
+</main>
