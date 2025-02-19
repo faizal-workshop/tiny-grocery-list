0ec1281794754332d82ff28441dfec3fa75aa5a0
Faizal Chan <moefc32@gmail.com>
2025-02-20 01:30:36 +0700

    fix fastify configurations

--- apps/backend/source/fastify.js
+++ apps/backend/source/fastify.js
@@ -1,24 +1,25 @@
 import fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
 import fastifyHelmet from '@fastify/helmet';
 import fastifyRateLimit from '@fastify/rate-limit';
-import fastifyCors from '@fastify/cors';
-import fastifyMultipart from '@fastify/multipart';
 
 const app = fastify({
     logger: true,
-    disableRequestLogging: true
+    disableRequestLogging: true,
 });
+
+app.register(fastifyCors);
 
 app.register(fastifyHelmet, {
     contentSecurityPolicy: false,
+    crossOriginOpenerPolicy: {
+        policy: 'same-origin-allow-popups',
+    },
 });
 
 app.register(fastifyRateLimit, {
     max: 100,
     timeWindow: '1 minute',
 });
-
-app.register(fastifyCors);
-app.register(fastifyMultipart);
 
 export default app;
