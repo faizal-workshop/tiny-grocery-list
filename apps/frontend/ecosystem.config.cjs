bbaeb4b5321cb6b768472b39a79c94541f7679be
Faizal Chan. <moefc32@gmail.com>
2025-02-22 01:33:45 +0700

    fix pm2 ecosystem, update packages

--- apps/frontend/ecosystem.config.cjs
+++ apps/frontend/ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
     apps: [
         {
             name: packageJson.name,
-            script: 'source/app.js',
+            script: 'build/app.js',
             env: {
                 HOST: process.env.VITE_IPBIND || '127.0.0.1',
                 PORT: parseInt(process.env.VITE_PORT, 10) || 4001,
